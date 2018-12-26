db28a0e458b275eab248038e5d6b424c239ecdaa
ColeSnyder <33235561+ColeSnyder@users.noreply.github.com>
2018-12-26 11:12:36 -0600

    Pushing before starting work on login via firebase.

--- dbconnect.js
+++ dbconnect.js
@@ -1,59 +1,55 @@
 // ALL FIREBASE INFO GOES HERE *******************************************************************************************
 
-    (function() {
-
+(function () {
     const config = {
-      apiKey: "AIzaSyA3JGscgdzANy5_gS_clTKeRPUUIfXwUVo",
-      authDomain: "logger-io-51f54.firebaseapp.com",
-      databaseURL: "https://logger-io-51f54.firebaseio.com",
-      projectId: "logger-io-51f54",
-      storageBucket: "logger-io-51f54.appspot.com",
-      messagingSenderId: "152590496413"
+        apiKey: "AIzaSyA3JGscgdzANy5_gS_clTKeRPUUIfXwUVo",
+        authDomain: "logger-io-51f54.firebaseapp.com",
+        databaseURL: "https://logger-io-51f54.firebaseio.com",
+        projectId: "logger-io-51f54",
+        storageBucket: "logger-io-51f54.appspot.com",
+        messagingSenderId: "152590496413" 
     };
-     firebase.initializeApp(config);
-  
-  const usernameInput = document.getElementById("usernameInput");
-  const passwordInput = document.getElementById("passwordInput");
-  const loginAttempt = document.getElementById("pressToLogin");
-  const signupAttempt = document.getElementById("SignUpAttempt");
+    firebase.initializeApp(config);
 
+    const usernameInput = document.getElementById("usernameInput");
+    const passwordInput = document.getElementById("passwordInput");
+    const loginAttempt = document.getElementById("pressToLogin");
+    const signupAttempt = document.getElementById("SignUpAttempt");
 
-  if(loginAttempt) {
+    if (loginAttempt) {
         loginAttempt.addEventListener('click', e => {
 
-        const user = usernameInput.value;
-        const pass = passwordInput.value;
-    
-        const auth = firebase.auth();
-    
-        const promise = auth.signInWithEmailAndPassword(user, pass);
-    
-        promise.catch(e => console.log(e.message));
-    
-        });
-}
+            const user = usernameInput.value;
+            const pass = passwordInput.value;
 
-  if(signupAttempt) {
+            const auth = firebase.auth();
+
+            const promise = auth.signInWithEmailAndPassword(user, pass);
+
+            promise.catch(e => console.log(e.message))
+        });
+    }
+
+    if (signupAttempt) {
         signupAttempt.addEventListener('click', e => {
 
-        const user = usernameInput.value;
-        const pass = passwordInput.value;
-      
-        const auth = firebase.auth();
-      
-        const promise = auth.createUserWithEmailAndPassword(user, pass);
-      
-        promise.catch(e => console.log(e.message));
-      
+            const user = usernameInput.value;
+            const pass = passwordInput.value;
+
+            const auth = firebase.auth();
+
+            const promise = auth.createUserWithEmailAndPassword(user, pass);
+
+            promise.catch(e => console.log(e.message))
         });
-  }
-
-  firebase.auth().onAuthStateChanged(firebaseUser => {
-    if(firebaseUser) {
-        console.log(firebaseUser);
-    } else {
-        console.log("not logged in");
     }
-  });
 
-    }());
+    firebase.auth().onAuthStateChanged(firebaseUser => {
+        if (firebaseUser) {
+            console.log(firebaseUser);
+        } else {
+            console.log("not logged in");
+        }
+    });
+
+}());
